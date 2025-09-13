aeb36471574e985f59775c97f590773a1f45b742
WassCodeur <wachioubouraima56@gmail.com>
2025-09-13 12:31:18 +0000

    Fix typing.ClassVar compatibility and reorganize report and favicon routes

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import typing
+if not hasattr(typing, "_ClassVar") and hasattr(typing, "ClassVar"):
+    typing._ClassVar = typing.ClassVar
+
 from send_email import send_sponsor_email
 from ticket import send_ticket_email
 from schedule import get_schedule, get_speaker_images, get_event_info
 
 
-if not hasattr(typing, "_ClassVar") and hasattr(typing, "ClassVar"):
-    typing._ClassVar = typing.ClassVar
 
 import os
 from dotenv import load_dotenv
@@ -157,13 +158,6 @@ educational_supporters = [
     sponsor for sponsor in paidsponsors if sponsor.get("level") == "educational"
 ]
 
-@app.route("/2025/report")
-def report():
-    return  redirect("https://report.pytogo.org/")
-
-@app.route("/favicon.ico")
-def favicon():
-    return app.send_static_file("favicon.ico")
 
 
 @app.route("/")
@@ -177,6 +171,15 @@ def home():
         proposal_closing_date=proposal_closing_date,
         paidsponsors=paidsponsors,
     )
+
+@app.route("/2025/report")
+def report():
+    return  redirect("https://report.pytogo.org/")
+
+@app.route("/favicon.ico")
+def favicon():
+    return app.send_static_file("favicon.ico")
+
 
 @app.route("/live")
 def live_page():
